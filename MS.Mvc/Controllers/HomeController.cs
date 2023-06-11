078d43072ad978f26958ec7cbdaeb6a236bb5380
Fores <Fores@DESKTOP-Q9GI0NO>
2023-06-11 22:13:10 +0300

    <e,e,e? ,f,f,f

--- MS.Mvc/Controllers/HomeController.cs
+++ MS.Mvc/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MS.Mvc.Models;
 using MS.Mvc.ViewModels;
@@ -7,13 +8,34 @@ namespace MS.Mvc.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly SignInManager<MSUser> signInManager;
+    private readonly UserManager<MSUser> userManager;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, SignInManager<MSUser> signInManager, UserManager<MSUser> userManager)
     {
         _logger = logger;
+        this.signInManager = signInManager;
+        this.userManager = userManager;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
+    {
+        if(signInManager.IsSignedIn(User)) 
+        {
+            var user = await userManager.GetUserAsync(User);
+            if (user.IsEmployee)
+            {
+                return RedirectToAction("Employee");
+            }
+            return RedirectToAction("Client");
+        }
+        return View();
+    }
+    public IActionResult Employee()
+    {
+        return View();
+    }
+    public IActionResult Client()
     {
         return View();
     }
---
+++ MS.Mvc/Data/Migrations/20230611184217_Migration004.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MS.Mvc.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration004 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmployee",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEmployee",
+                table: "AspNetUsers");
+        }
+    }
+}
